085f9de32ec4ff22b5d51121d58f774d9ab09d33
samhatter <sammyvincent2001@gmail.com>
2024-09-01 23:10:54 -0700

    fixed emblem problem

--- ui/src/Components/ConversationHeader.tsx
+++ ui/src/Components/ConversationHeader.tsx
@@ -18,15 +18,15 @@ const ConversationHeader: React.FC<ConversationHeaderProps> = (props) => {
             <Typography variant="h6" sx={{ padding: 0, color:'#eef'}}>
             {toggledConversation?.name || ''}
             </Typography>
-            <List sx={{ display: 'flex',  justifyContent: 'flex-start', flexDirection: 'row', padding: 0, margin: 0, height: '40px', width:"80%" }}>
+            <List sx={{ display: 'flex',  justifyContent: 'flex-start', flexDirection: 'row', padding: 0, margin: 0, height: '40px', width:"80" }}>
             {(toggledConversation ? toggledConversation.participants : []).sort().map((name, index)  => (
                 <ListItem key={index} sx={{ minWidth: '60px', padding: 1, margin: 0.5, width: "auto", borderRadius: '10px', backgroundColor: '#eef', borderTop: '1px solid #000', textAlign: 'center',}}>
                     <ListItemText primary={name} sx={{color:'#4A4455'}} />
                 </ListItem>
             ))}
             </List>
-            <Box sx={{ position: 'absolute', top: 8, right: -20, width: '100px' }}>
-                <img src='obsidianorder.svg' style={{width: '80%'}} />
+            <Box sx={{ position: 'absolute', top: 6, right: 0, width: '50px', height: 'auto' }}>
+                <img src='obsidianorder.svg' style={{width: '100%'}} />
             </Box>
     </Box>
   );
